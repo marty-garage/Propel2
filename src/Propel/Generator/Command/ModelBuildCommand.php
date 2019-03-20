44bcf264e1717b1b7b067cf04d36d26195f5ac62
marty-garage <48030945+marty-garage@users.noreply.github.com>
2019-03-20 21:36:50 +0100

    Update ModelBuildCommand.php

--- src/Propel/Generator/Command/ModelBuildCommand.php
+++ src/Propel/Generator/Command/ModelBuildCommand.php
@@ -164,6 +164,38 @@ class ModelBuildCommand extends AbstractCommand
         
         $xml->asXML(array_keys($manager->getSchemas())[0]);
         
+        //-------------------------
+        //TODO:test this
+        preg_match('(.+[/\\]models[/\\])(.+[/\\]).+', basename(array_keys($manager->getSchemas())[0]),$models_path);//array_keys($manager->getSchemas())
+       
+        $autoload_location = __DIR__.'/../../../vendor/autoload.php';
+        $template = "<?php
+
+class ci_propel_autoloader {
+
+    public function __construct() {
+        $this->init_autoloader();
+    }
+
+    private function init_autoloader(){
+        spl_autoload_register(function($classname){
+            require({$autoload_location});
+            ";
+            
+            foreach($xml->table as $table){
+                $template+=
+                "
+                require(APPPATH.'models/".$models_path[2]."generated-reversed-database/generated-classes/Base/".$table->getName().".php');";
+            }
+            $template+=
+            "      }); 
+                }
+            }";
+        file_put_contents($models_path[1].'ci_propel_autoloader.php', $template, LOCK_EX);
+        
+        
+        //---------------------
+        
         $manager->setLoggerClosure(function ($message) use ($input, $output) {
             if ($input->getOption('verbose')) {
                 $output->writeln($message);
